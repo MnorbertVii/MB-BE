bbf515f6435ee0856c33f01761a0989151548fb4
NIRUGIRA MUHIZI Norbert <norbertmuhizi21@gmail.com>
2024-04-10 19:09:49 +0200

    worked on testing the apis

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -1,5 +1,8 @@
 import request from "supertest";
 import app from "../src/app";
+import FormData from "form-data";
+import fs from "fs";
+import path from "path";
 
 const TOKEN: string = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGNiMTViN2IxNmNiODJjZTMyYjVmNCIsImVtYWlsIjoibmltdWhub3JiZXJ0QGdtYWlsLmNvbSIsImlhdCI6MTcxMjc1MDQyOCwiZXhwIjoxNzE1MzQyNDI4fQ.6j43aF1Ihv2nVZOzk4v8TJBGOz5Ai4779_CYhzcq6Oc";
 const num: number = Math.floor(Math.random() * 10000);
@@ -9,13 +12,52 @@ test("get articles", async () => {
 	expect(response.statusCode).toBe(200);
 });
 
+test("get messages", async () => {
+    const response = await request(app).get("/messages").send().set("Authorization",TOKEN);
+    expect(response.statusCode).toBe(200);
+});
+
+test("get users", async () => {
+    const response = await request(app).get("/users").send().set("Authorization",TOKEN);
+    expect(response.statusCode).toBe(200);
+});
+
+test("Resource NOT FOUND", async () => {
+    const response = await request(app).get("/art").send();
+    expect(response.statusCode).toBe(404);
+})
+
+test("post signup ", async () => {
+    const response = await request(app).post("/users/signup").send({
+        fullName: "user",
+        email: "user" + num + "@gmail.com",
+        password: "user123"
+    }
+    );
+    expect(response.statusCode).toBe(200);
+})
+
+test("post login", async () => {
+    const response = await request(app).post("/users/login").send(
+        { email:"user6711@gmail.com",
+        password:"user123"
+        }
+    );
+    expect(response.statusCode).toBe(200);
+})
+
+
+
 
 // test ("create article", async () => {
-// 	const response = await request(app).post("/articles").set("Authorization", TOKEN).send({
-// 		title: "test article title",
-// 		content: "test article content",
-// 		image: "https://th.bing.com/th/id/R.3d88a927f8529dcba03364b09d98adbe?rik=JYmQaMVSULpYQg&riu=http%3a%2f%2fthewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2fnature-images.jpg&ehk=BNPsuSOUR7ATZ3EpRwxx1xFl7LUbO3tYlu1wFLCBrCE%3d&risl=&pid=ImgRaw&r=0"
-// 	});
-// 	expect(response.statusCode).toBe(201);
-// })
+// 	const req =  request(app).post("/articles").set("Authorization", TOKEN)
+// 	.field("title", "test article title")
+//     .field("content", "test article content")
+//     .attach("image", fs.createReadStream(path.join(__dirname, "images/avatar_1 - Copy.jpg")));
+// 	req.on('error', err => console.log(err));
 
+// 	const response = await req;
+
+// 	console.log(response);
+// 	expect(response.statusCode).toBe(201);
+// });
