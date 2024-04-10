c28a530dac647e50eaeeb8a11aea143ee0a3a94f
NIRUGIRA MUHIZI Norbert <norbertmuhizi21@gmail.com>
2024-04-10 19:55:00 +0200

    modified a test suite

--- test/jest.test.ts
+++ test/jest.test.ts
@@ -8,59 +8,59 @@ const TOKEN: string =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGNiMTViN2IxNmNiODJjZTMyYjVmNCIsImVtYWlsIjoibmltdWhub3JiZXJ0QGdtYWlsLmNvbSIsImlhdCI6MTcxMjc1MDQyOCwiZXhwIjoxNzE1MzQyNDI4fQ.6j43aF1Ihv2nVZOzk4v8TJBGOz5Ai4779_CYhzcq6Oc";
 const num: number = Math.floor(Math.random() * 10000);
 
-// test("get articles", async () => {
-// 	const response = await request(app).get("/articles").send();
-// 	expect(response.statusCode).toBe(200);
-// });
+test("get articles", async () => {
+	const response = await request(app).get("/articles").send();
+	expect(response.statusCode).toBe(200);
+});
 
-// test("get messages", async () => {
-//     const response = await request(app).get("/messages").send().set("Authorization",TOKEN);
-//     expect(response.statusCode).toBe(200);
-// });
+test("get messages", async () => {
+    const response = await request(app).get("/messages").send().set("Authorization",TOKEN);
+    expect(response.statusCode).toBe(200);
+});
 
-// test("get users", async () => {
-//     const response = await request(app).get("/users").send().set("Authorization",TOKEN);
-//     expect(response.statusCode).toBe(200);
-// });
+test("get users", async () => {
+    const response = await request(app).get("/users").send().set("Authorization",TOKEN);
+    expect(response.statusCode).toBe(200);
+});
 
-// test("Resource NOT FOUND", async () => {
-//     const response = await request(app).get("/art").send();
-//     expect(response.statusCode).toBe(404);
-// })
+test("Resource NOT FOUND", async () => {
+    const response = await request(app).get("/art").send();
+    expect(response.statusCode).toBe(404);
+})
 
-// test("post signup ", async () => {
-//     const response = await request(app).post("/users/signup").send({
-//         fullName: "user",
-//         email: "user" + num + "@gmail.com",
-//         password: "user123"
-//     }
-//     );
-//     expect(response.statusCode).toBe(200);
-// })
+test("post signup ", async () => {
+    const response = await request(app).post("/users/signup").send({
+        fullName: "user",
+        email: "user" + num + "@gmail.com",
+        password: "user123"
+    }
+    );
+    expect(response.statusCode).toBe(200);
+})
 
-// test("post login", async () => {
-//     const response = await request(app).post("/users/login").send(
-//         { email:"user6711@gmail.com",
-//         password:"user123"
-//         }
-//     );
-//     expect(response.statusCode).toBe(200);
-// })
+test("post login", async () => {
+    const response = await request(app).post("/users/login").send(
+        { email:"user6711@gmail.com",
+        password:"user123"
+        }
+    );
+    expect(response.statusCode).toBe(200);
+})
 
 
-// test("create article", async () => {
-//   try {
-//     const imagePath = path.join(__dirname, "images/avatar_1 - Copy.jpg");
+test("create article", async () => {
+  try {
+    const imagePath = path.join(__dirname, "images/avatar_1 - Copy.jpg");
 
-//     const response = await request(app)
-//       .post("/articles")
-//       .set("Authorization", TOKEN)
-//       .field("title", "test article title")
-//       .field("content", "test article content")
-//       .attach("image", imagePath);
-//     console.log(response);
-//     expect(response.statusCode).toBe(201);
-//   } catch (error) {
-//     console.error(`Error: ${error}`);
-//   }
-// });
+    const response = await request(app)
+      .post("/articles")
+      .set("Authorization", TOKEN)
+      .field("title", "test article title")
+      .field("content", "test article content")
+      .attach("image", imagePath);
+    console.log(response);
+    expect(response.statusCode).toBe(201);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+});
